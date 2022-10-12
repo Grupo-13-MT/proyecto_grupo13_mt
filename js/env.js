dfea3fbdb9bedb82800d0100c2e0a14fc56b63d3
Grupo-13-MT <=>
2022-10-11 20:18:40 -0500

    chg IP

--- js/env.js
+++ js/env.js
@@ -1,4 +1,4 @@
 const env = {
-    url: 'http://129.153.88.157/f0910224/proyecto_grupo13_mt',
+    url: 'http://129.146.190.11/f1110222/proyecto_grupo13_mt',
     api: 'https://ge98b58f620aa8b-jk6qaf05q3cydvl5.adb.us-phoenix-1.oraclecloudapps.com/ords/admin'
 }
